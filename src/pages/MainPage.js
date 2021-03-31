1b63c46d7264fd409903aa75ee0ef93c2f4feab5
Dr-Positive <Crysisdan@gmail.com>
2021-03-31 18:46:51 +0500

    design 0.1
    добавлены поля регистрации

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { TextField } from '@material-ui/core';
 
 const drawerWidth = 240;
 
@@ -32,6 +39,8 @@ const useStyles = makeStyles((theme) => ({
 
 
 
+
+
 export default function MainPage() {
   const classes = useStyles();
 
@@ -54,9 +63,51 @@ export default function MainPage() {
         anchor="right"
       >        
         
-          <Button  color ="inherit" variant="ouylined">Зарегистроваться</Button>
+          <ButtonGroup disableElevation variant="contained" color="primary">
+             <Button>Вход</Button>
+             <Button>Регистрация</Button>
+          </ButtonGroup>
+          <TextField
+            variant="filled"
+            color="secondary"
+            type="name"
+            label="Ваше имя"
+            placeholder="Имя"          
+          />
+           <TextField
+            variant="filled"
+            color="secondary"
+            type="name"
+            label="Ваша фамилия"
+            placeholder="Фамилия"
           
-          <Button color="secondary" variant="contained">Войти</Button>
+          />
+           <TextField
+            variant="filled"
+            color="secondary"
+            type="login"
+            label="Логин"
+            placeholder="Логин"
+          
+          />
+           <TextField
+            variant="filled"
+            color="secondary"
+            type="email"
+            label="Ваша почта"
+            placeholder="Почта"
+          
+          />
+           <TextField
+            variant="filled"
+            color="secondary"
+            type="password"
+            label="Пароль"
+            placeholder="Пароль"
+          
+          />
+          
+          <Button color="primary" variant="contained">Зарегистрироваться</Button>
           
         
       </Drawer>
