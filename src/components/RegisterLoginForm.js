d5a4545a5dd838934b384ea44565f0ca4dc2c399
Aceki <aceki320@gmail.com>
2021-04-30 10:19:00 +0500

    Separate styles. Update 'react-scripts'

--- src/components/RegisterLoginForm.js
+++ src/components/RegisterLoginForm.js
@@ -5,123 +5,11 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
 
-import { makeStyles } from '@material-ui/core/styles';
-
 import TabPanel from "./TabPanel";
 
-const useStyles = makeStyles((theme) => ({
-    container: {
-        display: "flex",
-        minWidth: "290px",
-        maxWidth: "420px",
-        flexDirection: "column",
-        alignItems: "center",
-        backgroundColor: "#395273",
-        borderRadius: "13px"
-    },
-    logo: {
-        width: "220px",
-        textAlign: "center",
-        color: "white",
-    },
-    logoName: {
-        margin: "0",
-        marginTop: "10px",
-        fontFamily: "Monteserrat, sans-serif",
-        fontWeight: "bold",
-        fontSize: "42px"
-    },
-    actionTabs: {
-        color: "white",
-        width: "100%",
-        '& .MuiTabs-indicator': {
-            backgroundColor: "white"
-        }
-    },
-    logoCaption: {
-        margin: "0",
-        fontSize: "15px"
-    },
-    panelRegister: {
-        boxSizing: "border-box",
-        padding: "15px",
-        width: "100%"
-    },
-    registerFields: {
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr 1fr",
-        gap: "15px",
-        '& :nth-child(n)': {
-            gridColumn: "span 3"
-        },
-        '& :nth-child(3)': {
-            gridColumn: "span 1"
-        },
-        '& :nth-child(4)': {
-            gridColumn: "span 1"
-        },
-        '& :nth-child(5)': {
-            gridColumn: "span 1"
-        }
-    },
-    panelLogin: {
-        boxSizing: "border-box",
-        padding: "15px",
-        width: "100%"
-    },
-    loginFields: {
-        display: "grid",
-        gridTemplateColumns: "1fr",
-        gap: "15px"
-    },
-    field: {
-        width: "100%",
-        '& .MuiFormLabel-root': {
-            color: "#919BA6"
-        },
-        '& .MuiFormLabel-root.Mui-focused': {
-            color: "white"
-        },
-        '& .MuiFilledInput-underline:after': {
-            borderBottomColor: "white"
-        },
-        '& .MuiInputBase-input': {
-            color: "white"
-        }
-    },
-    registerButton: {
-        margin: "0",
-        marginTop: "20px",
-        width: "100%",
-        backgroundColor: "#00FF00",
-        color: "white",
-        '&:hover': {
-            backgroundColor: "white",
-            color: "black"
-        },
-        '& .MuiButtonBase-root': {
-            backgroundColor: "white"
-        }
-    },
-    loginButton: {
-        margin: "0",
-        marginTop: "20px",
-        width: "100%",
-        backgroundColor: "#1463FF",
-        color: "white",
-        '&:hover': {
-            backgroundColor: "white",
-            color: "black"
-        },
-        '& .MuiButtonBase-root': {
-            backgroundColor: "white"
-        }
-    }
-}));
+import styles from './RegisterLoginForm.module.css';
 
 function RegisterLoginForm(props) {
-    const classes = useStyles();
-
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
@@ -130,33 +18,33 @@ function RegisterLoginForm(props) {
 
     return (
         <div className={props.className}>
-            <div className={classes.container}>
-                <div className={classes.logo}>
-                    <p className={classes.logoName}>Graph App</p>
-                    <p className={classes.logoCaption}>Редактирование графов в реальном времени</p>
+            <div className={styles.container}>
+                <div className={styles.logo}>
+                    <p className={styles.logoName}>Graph App</p>
+                    <p className={styles.logoCaption}>Редактирование графов в реальном времени</p>
                 </div>
-                <Tabs value={value} onChange={handleChange} className={classes.actionTabs} variant="fullWidth" fixed>
+                <Tabs value={value} onChange={handleChange} className={styles.actionTabs} variant="fullWidth" fixed>
                     <Tab label="Регистрация" />
                     <Tab label="Вход" />
                 </Tabs>
-                <TabPanel value={value} index={0} className={classes.panelRegister}>
-                    <div className={classes.registerFields}>
-                        <TextField className={classes.field} variant="filled" type="text" label="Email" required />
-                        <TextField className={classes.field} variant="filled" type="text" label="Логин" required />
-                        <TextField className={classes.field} variant="filled" type="text" label="Фамилия" required />
-                        <TextField className={classes.field} variant="filled" type="text" label="Имя" required />
-                        <TextField className={classes.field} variant="filled" type="text" label="Отчество" required />
-                        <TextField className={classes.field} variant="filled" type="password" label="Пароль" required />
-                        <TextField className={classes.field} variant="filled" type="password" label="Повтор пороля" required />
+                <TabPanel value={value} index={0} className={styles.panelRegister}>
+                    <div className={styles.registerFields}>
+                        <TextField className={styles.field} variant="filled" type="text" label="Email" required />
+                        <TextField className={styles.field} variant="filled" type="text" label="Логин" required />
+                        <TextField className={styles.field} variant="filled" type="text" label="Фамилия" required />
+                        <TextField className={styles.field} variant="filled" type="text" label="Имя" required />
+                        <TextField className={styles.field} variant="filled" type="text" label="Отчество" required />
+                        <TextField className={styles.field} variant="filled" type="password" label="Пароль" required />
+                        <TextField className={styles.field} variant="filled" type="password" label="Повтор пороля" required />
                     </div>
-                    <Button className={classes.registerButton}>Зарегистрироваться</Button>
+                    <Button className={styles.registerButton}>Зарегистрироваться</Button>
                 </TabPanel>
-                <TabPanel value={value} index={1} className={classes.panelLogin}>
-                    <div className={classes.loginFields}>
-                        <TextField className={classes.field} variant="filled" type="text" label="Логин" required />
-                        <TextField className={classes.field} variant="filled" type="password" label="Пароль" required />
+                <TabPanel value={value} index={1} className={styles.panelLogin}>
+                    <div className={styles.loginFields}>
+                        <TextField className={styles.field} variant="filled" type="text" label="Логин" required />
+                        <TextField className={styles.field} variant="filled" type="password" label="Пароль" required />
                     </div>
-                    <Button className={classes.loginButton}>Войти</Button>
+                    <Button className={styles.loginButton}>Войти</Button>
                 </TabPanel>
             </div>
         </div>
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,78 +1,27 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Typography } from '@material-ui/core/';
+import AppBar from '@material-ui/core/AppBar';
 
 import RegisterLoginForm from '../components/RegisterLoginForm';
 
-const useStyles = makeStyles((theme) => ({
-    root: { 
-        backgroundImage: "url(https://psv4.userapi.com/c520036/u76983384/docs/d12/1a3651a8e1df/41589-2-graphic-elements-png-image-high-quality.png?extra=t_YROjFz7F1F5vELCnEdd5mBl3zhF59BfBUufV3B62HT4Te8FMe-Wv36S3TXVeqo5I4EGp3t_bCjhaYCSHL5Rotk5aZKQw2lJdwYq3cNE5XVuS0icbC5ySNwmOhPgBi0rHXQ9eZ7c9IxEEcRHAO8Xw)",
-        backgroundSize: "cover"
-    },
-    appBar: {
-        height: "40px"
-    },
-    barLogo: {
-        textDecoration: "none",
-        fontFamily: "Montserrat, sans-serif",
-        fontSize: "18px",
-        fontWeight: "bold",
-        color: "white",
-        marginTop: "auto",
-        marginBottom: "auto",
-        marginLeft: "20px"
-    },
-    content: {
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr", // 100px 2fr 1fr 100px // 150px 1fr 350px 150px
-        gridTemplateAreas: "'a b'",
-        justifyContent: "center",
-        alignContent: "center",
-        height: "100vh"
-    },
-    form: {
-        gridArea: "b",
-        margin: "0 auto"
-    },
-    demo: {
-        gridArea: "a",
-        alignSelf: "center",
-        margin: "0 auto"
-    },
-    screenshot: {
-        display: "block",
-        width: "640px",
-        height: "360px",
-        border: "solid 2px",
-        borderColor: "black",
-        margin: "0 auto"
-    },
-    screenshotCaption: {
-        textAlign: "center",
-        maxWidth: "640px",
-        minWidth: "360px"
-    }
-}));
+import styles from './MainPage.module.css';
 
 function MainPage() {
-    const classes = useStyles();
-
     return (
-        <div className={classes.root}>
+        <div className={styles.root}>
             <header>
-                <AppBar position="fixed" className={classes.appBar}>
-                    <a href="/" className={classes.barLogo}>Graph App</a>
+                <AppBar position="fixed" className={styles.appBar}>
+                    <a href="/" className={styles.barLogo}>Graph App</a>
                 </AppBar>  
             </header> 
-            <main className={classes.content}>
-                <div className={classes.demo}>
-                    <img className={classes.screenshot} src="https://mobiltelefon.ru/photo/february21/05/nothing_naznachila_anons_na_sleduuschuu_nedelu_i_otozvala_ego_picture2_0.jpg" />
-                    <p className={classes.screenshotCaption}>
+            <main className={styles.content}>
+                <div className={styles.demo}>
+                    <img className={styles.screenshot} src="https://mobiltelefon.ru/photo/february21/05/nothing_naznachila_anons_na_sleduuschuu_nedelu_i_otozvala_ego_picture2_0.jpg" />
+                    <p className={styles.screenshotCaption}>
                         <b>Graph App</b> — интерактивный редактор графов, предназначенный для визуализации больших объёмов данных и исследования различных предметных областей.
                     </p>
                 </div>
-                <RegisterLoginForm className={classes.form} />
+                <RegisterLoginForm className={styles.form} />
             </main>
         </div>
     );
